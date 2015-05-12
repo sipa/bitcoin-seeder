6e5a39185e2eec22256df8c612fe0e29f6cd42e8
Jonas Schnelli <jonas.schnelli@include7.ch>
2015-05-12 20:18:33 +0200

    avoid connecting to non valid ipv4 ranges (>=240.0.0.0)

--- netbase.h
+++ netbase.h
@@ -47,6 +47,7 @@ class CNetAddr
         bool SetSpecial(const std::string &strName); // for Tor and I2P addresses
         bool IsIPv4() const;    // IPv4 mapped address (::FFFF:0:0/96, 0.0.0.0/0)
         bool IsIPv6() const;    // IPv6 address (not mapped IPv4, not Tor/I2P)
+        bool IsReserved() const; // Against Hetzners Abusal/Netscan Bot
         bool IsRFC1918() const; // IPv4 private networks (10.0.0.0/8, 192.168.0.0/16, 172.16.0.0/12)
         bool IsRFC3849() const; // IPv6 documentation address (2001:0DB8::/32)
         bool IsRFC3927() const; // IPv4 autoconfig (169.254.0.0/16)
