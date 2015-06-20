01c6b4e4c25a642496a02944b0fe0ad924e15d1e
Peter Todd <pete@petertodd.org>
2015-06-20 18:21:06 -0400

    Update list of testnet seeds

--- main.cpp
+++ main.cpp
@@ -340,7 +340,11 @@ extern "C" void* ThreadStats(void*) {
 }
 
 static const string mainnet_seeds[] = {"dnsseed.bluematt.me", "bitseed.xf2.org", "dnsseed.bitcoin.dashjr.org", "seed.bitcoin.sipa.be", ""};
-static const string testnet_seeds[] = {"testnet-seed.bitcoin.petertodd.org", "static-testnet-seed.bitcoin.petertodd.org", ""};
+static const string testnet_seeds[] = {"testnet-seed.alexykot.me",
+                                       "testnet-seed.bitcoin.petertodd.org",
+                                       "testnet-seed.bluematt.me",
+                                       "testnet-seed.bitcoin.schildbach.de",
+                                       ""};
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
