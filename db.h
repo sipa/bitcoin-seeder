98f98c2a3a1c9a6f84dc773fb5dfe949bdddc8d6
Pieter Wuille <pieter.wuille@gmail.com>
2015-07-29 15:16:26 +0200

    Update version/height requirements

--- db.h
+++ db.h
@@ -12,11 +12,11 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 40000
+#define REQUIRE_VERSION 70001
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 230000;
+    return testnet ? 500000 : 350000;
 }
 
 std::string static inline ToString(const CService &ip) {
