d0a16a6cc0ff86bf163a6747619ff063560295aa
Kevin Gallagher <kevin@z.cash>
2016-09-06 22:42:48 -0700

    Update magic numbers for Zcash
    Source: https://github.com/zcash/zcash/blob/zc.v0.11.2.latest/src/chainparams.cpp#L154

--- main.cpp
+++ main.cpp
@@ -450,10 +450,10 @@ int main(int argc, char **argv) {
   bool fDNS = true;
   if (opts.fUseTestNet) {
       printf("Using testnet.\n");
-      pchMessageStart[0] = 0x0b;
-      pchMessageStart[1] = 0x11;
-      pchMessageStart[2] = 0x09;
-      pchMessageStart[3] = 0x07;
+      pchMessageStart[0] = 0x26;
+      pchMessageStart[1] = 0xA7;
+      pchMessageStart[2] = 0x24;
+      pchMessageStart[3] = 0xB6;
       seeds = testnet_seeds;
       fTestNet = true;
   }
