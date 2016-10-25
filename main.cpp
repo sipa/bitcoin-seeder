338d209c4a7130f15b28b3b774b5eb3f44cd298d
Kevin Gallagher <kevingallagher@gmail.com>
2016-10-25 15:12:08 -0700

    Removes Bitcoin-related Tor hidden service seed

--- main.cpp
+++ main.cpp
@@ -394,9 +394,9 @@ static const string testnet_seeds[] = {"betatestnet.z.cash", "explorer.testnet.z
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
-  if (!fTestNet){
-    db.Add(CService("kjy2eqzk4zwi5zd3.onion", 8333), true);
-  }
+  //if (!fTestNet){
+  //  db.Add(CService("kjy2eqzk4zwi5zd3.onion", 8333), true);
+  //}
   do {
     for (int i=0; seeds[i] != ""; i++) {
       vector<CNetAddr> ips;
