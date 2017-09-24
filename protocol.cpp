a917fa42eece89ce8a0d047bef237e475865fdb2
bitcoinz <r6fup8oxo9yi3vqcpkn@gmail.com>
2017-09-24 02:17:56 -0700

    msg

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0x24, 0xE9, 0x27, 0x64 };
+unsigned char pchMessageStart[4] = { 0x24, 0xe9, 0x27, 0x64 };
 
 CMessageHeader::CMessageHeader()
 {
