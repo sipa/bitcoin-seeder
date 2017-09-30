8b196d53b0d21490c71c4e7105ee930b72c0878a
bitcoinz <r6fup8oxo9yi3vqcpkn@gmail.com>
2017-09-30 05:22:58 -0700

    new seeder: dnsseed.kemperink.org

--- main.cpp
+++ main.cpp
@@ -389,7 +389,7 @@ extern "C" void* ThreadStats(void*) {
   } while(1);
 }
 
-static const string mainnet_seeds[] = {"btzseed.blockhub.info", "bitcoinz.ph", "zparty.pl", ""};
+static const string mainnet_seeds[] = {"btzseed.blockhub.info", "bitcoinz.ph", "zparty.pl", "dnsseed.kemperink.org", ""};
 static const string testnet_seeds[] = {""};
 static const string *seeds = mainnet_seeds;
 
