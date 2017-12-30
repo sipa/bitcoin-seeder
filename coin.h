14c2dbd8add93c1c8a209370ebf18f3f50a932ba
Lucas Betschart <lucasbetschart@gmail.com>
2017-12-30 23:34:08 +0100

    Use seeds from current Bitcoin Core

--- coin.h
+++ coin.h
@@ -3,11 +3,21 @@
 
 #include <string>
 
-static const std::string mainnet_seeds[] = {"dnsseed.bluematt.me", "bitseed.xf2.org", "dnsseed.bitcoin.dashjr.org", "seed.bitcoin.sipa.be", ""};
-static const std::string testnet_seeds[] = {"testnet-seed.alexykot.me",
-                                            "testnet-seed.bitcoin.petertodd.org",
-                                            "testnet-seed.bluematt.me",
-                                            "testnet-seed.bitcoin.schildbach.de",
+static const std::string mainnet_seeds[] = {"seed.bitcoin.sipa.be", // Pieter Wuille, only supports x1, x5, x9, and xd
+                                            "dnsseed.bluematt.me", // Matt Corallo, only supports x9
+                                            "dnsseed.bitcoin.dashjr.org", // Luke Dashjr
+                                            "seed.bitcoinstats.com", // Christian Decker, supports x1 - xf
+                                            "seed.bitcoin.jonasschnelli.ch", // Jonas Schnelli, only supports x1, x5, x9, and xd
+                                            "seed.btc.petertodd.org", // Peter Todd, only supports x1, x5, x9, and xd
+                                            "seed.bitcoin.sprovoost.nl", // Sjors Provoost
+                                            "dnsseed.emzy.de", // Stephan Oeste
+                                            "seed.bitcoin.wiz.biz", // Jason Maurice
+                                            ""};
+
+static const std::string testnet_seeds[] = {"testnet-seed.bitcoin.jonasschnelli.ch",
+                                            "seed.tbtc.petertodd.org",
+                                            "seed.testnet.bitcoin.sprovoost.nl",
+                                            "testnet-seed.bluematt.me", // Just a static list of stable node(s), only supports x9
                                             ""};
 
 static const int mainnet_port = 8333;
