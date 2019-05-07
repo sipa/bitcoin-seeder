af3bfa04319000705a38a53936afb527bcaeff88
blockOcean <50343687+blockOcean@users.noreply.github.com>
2019-05-06 19:57:41 -0500

    Riecoin pchMessageStart changes

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0xfc, 0xbc, 0xb2, 0xdb };
 
 CMessageHeader::CMessageHeader()
 {
