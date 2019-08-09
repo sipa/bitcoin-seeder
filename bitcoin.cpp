36bd7f9a0f14b79e889715a05d71be1035f2a96b
Pieter Wuille <pieter.wuille@gmail.com>
2019-08-09 16:56:23 -0700

    Send BIP37 fRelay as false

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -81,7 +81,8 @@ class CNode {
     BeginMessage("version");
     int nBestHeight = GetRequireHeight();
     string ver = "/bitcoin-seeder:0.01/";
-    vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight;
+    uint8_t fRelayTxs = 0;
+    vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight << fRelayTxs;
     EndMessage();
   }
  
