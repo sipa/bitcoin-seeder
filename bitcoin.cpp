84d192b4f2917921bf87c8e1aabc1b2b4022dba8
cryptozeny <cryptozeny@gmail.com>
2019-08-22 06:49:50 +0900

    Sugarchain: bump version to v0.1.1.3

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -80,7 +80,7 @@ class CNode {
     CAddress me(CService("0.0.0.0"));
     BeginMessage("version");
     int nBestHeight = GetRequireHeight();
-    string ver = "/sugarchain-seeder:0.1.1.2/";
+    string ver = "/sugarchain-seeder:0.1.1.3/";
     vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight;
     EndMessage();
   }
