24e1cac582fad0a2ec06af2e54a55af947185796
Pieter Wuille <pieter.wuille@gmail.com>
2020-10-09 19:51:19 -0700

    Avoid strncpy for commands

--- protocol.cpp
+++ protocol.cpp
@@ -36,7 +36,9 @@ CMessageHeader::CMessageHeader()
 CMessageHeader::CMessageHeader(const char* pszCommand, unsigned int nMessageSizeIn)
 {
     memcpy(pchMessageStart, ::pchMessageStart, sizeof(pchMessageStart));
-    strncpy(pchCommand, pszCommand, COMMAND_SIZE);
+    int command_len = strlen(pszCommand);
+    memcpy(pchCommand, pszCommand, command_len);
+    memset(pchCommand + command_len, 0, COMMAND_SIZE - command_len);
     nMessageSize = nMessageSizeIn;
     nChecksum = 0;
 }
