d0b3d729e23de735a426af1438573dca3319f20c
Sjors Provoost <sjors@sprovoost.nl>
2021-09-17 18:16:54 +0200

    Bump testnet and mainnet minimum height

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 500000 : 350000;
+    return testnet ? 700000 : 2000000;
 }
 
 std::string static inline ToString(const CService &ip) {
